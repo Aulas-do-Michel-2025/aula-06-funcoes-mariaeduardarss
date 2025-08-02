3dbe2255bedbe1c851d6c8f97a67caebb78b9eba
Maria Eduarda <mariaeduardarsouza@outlook.com>
2025-08-02 14:21:17 -0300

    exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -13,9 +13,3 @@ def calcular_media(lista):
     if len(lista) == 0:
         return 0 
     return sum(lista) / len(lista)
-
-numeros = input("Digite os números separados por vírgula: ")
-lista_numeros = [float(num.strip()) for num in numeros.split(",")]
-media = calcular_media(lista_numeros)
-
-print(f"A média dos valores é: {media}")
